ee5b2a2f9ae0b0804097fccf6dcd2998e708b5f9
Bokul <bokul@horoppa.com>
2012-11-12 22:23:46 +0600

    reformate code

--- application/controllers/userpanel.php
+++ application/controllers/userpanel.php
@@ -1,11 +1,13 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-include_once(APPPATH.'libraries/User_Controller.php');
+include_once(APPPATH . 'libraries/User_Controller.php');
 
-class Userpanel extends User_Controller {
+class Userpanel extends User_Controller
+{
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         $this->load->helper('url');
     }
@@ -17,7 +19,7 @@ class Userpanel extends User_Controller {
         $data = null;
         $error = null;
         $title = 'Campaign';
-        $this->template->write_view('content','template/user/pages/campaign',array('data'=>$data,'error'=>$error,'title'=>$title));
+        $this->template->write_view('content', 'template/user/pages/campaign', array('data' => $data, 'error' => $error, 'title' => $title));
         $this->template->render();
     }
 
@@ -28,298 +30,300 @@ class Userpanel extends User_Controller {
         $error = null;
         $title = 'Account Details';
         $user_info = $this->session->userdata('user_info');
-		$user_id=$user_info['user_id'];
-		$this->load->model('user_panelmodel');
-		$data=$this->user_panelmodel->user_data($user_id);
-		$this->template->write_view('content','template/user/pages/account_details',array('data'=>$data,'error'=>$error,'title'=>$title));
+        $user_id = $user_info['user_id'];
+        $this->load->model('user_panelmodel');
+        $data = $this->user_panelmodel->user_data($user_id);
+        $this->template->write_view('content', 'template/user/pages/account_details', array('data' => $data, 'error' => $error, 'title' => $title));
         $this->template->render();
-        $this->output->enable_profiler(TRUE);
+        //$this->output->enable_profiler(TRUE);
     }
-	public function update_details()
-	{
-	  $this->load->helper('form');
-	  $error = null;
-      $title = 'Account Details Update';
-      $data = $this->input->post();
-	  $this->load->model('user_panelmodel');
-     if($this->user_panelmodel->user_update($data))
-	{
-		   $msg = array(
-                        'status' => true,
-                        'class' => 'successbox',
-                        'msg' => 'Account Details Update Successfully.'
-                    );
 
-                    $data = json_encode($msg);
+    public function update_details()
+    {
+        $this->load->helper('form');
+        $error = null;
+        $title = 'Account Details Update';
+        $data = $this->input->post();
+        $this->load->model('user_panelmodel');
+        if ($this->user_panelmodel->user_update($data)) {
+            $msg = array(
+                'status' => true,
+                'class' => 'successbox',
+                'msg' => 'Account Details Update Successfully.'
+            );
 
-                    $this->session->set_flashdata('msg', $data);
-	}
-	 else
-                {
-                    $msg = array(
-                        'status' => false,
-                        'class' => 'errormsgbox',
-                        'msg' => 'Account Details Update Failed please try again.'
-                    );
+            $data = json_encode($msg);
 
-                    $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
+        }
+        else
+        {
+            $msg = array(
+                'status' => false,
+                'class' => 'errormsgbox',
+                'msg' => 'Account Details Update Failed please try again.'
+            );
 
-                    $this->session->set_flashdata('msg', $data);
-				}
-	
-	 redirect('userpanel/account_details');
-	  
-	 
-	}
-	 public function setting_interface()
+            $data = json_encode($msg);
+
+            $this->session->set_flashdata('msg', $data);
+        }
+
+        redirect('userpanel/account_details');
+
+    }
+
+    public function setting_interface()
     {
         $this->load->helper('form');
         $data = null;
         $error = null;
         $title = 'Setting Interface';
-         $this->template->write_view('content','template/user/pages/settinginterface',array('data'=>$data,'error'=>$error,'title'=>$title));
+        $this->template->write_view('content', 'template/user/pages/settinginterface', array('data' => $data, 'error' => $error, 'title' => $title));
         $this->template->render();
-        $this->output->enable_profiler(TRUE);
+        //$this->output->enable_profiler(TRUE);
     }
-	
-	
-	public function add_interface()
-	{
-	  $this->load->helper('form');
-	  $error = null;
-      $title = 'Interface Add';
-      $data = $this->input->post();
-	  $user_info = $this->session->userdata('user_info');
-	  $data['user_id']=$user_info['user_id'];
-	  $this->load->model('user_panelmodel');
-	  if($this->user_panelmodel->create_interface($data))
-	  { 
-	   $msg = array(
-                        'status' => true,
-                        'class' => 'successbox',
-                        'msg' => 'Interface  Added successfully.'
-                    );
 
-        $data = json_encode($msg);
-        $this->session->set_flashdata('msg', $data);
-	  }	
-	  else
-	  {
-        $msg = array(
-                        'status' => false,
-                        'class' => 'errormsgbox',
-                        'msg' => 'Interface  Added Fail.'
-                    );
 
-        $data = json_encode($msg);
-        $this->session->set_flashdata('msg', $data);
+    public function add_interface()
+    {
+        $this->load->helper('form');
+        $error = null;
+        $title = 'Interface Add';
+        $data = $this->input->post();
+        $user_info = $this->session->userdata('user_info');
+        $data['user_id'] = $user_info['user_id'];
+        $this->load->model('user_panelmodel');
+        if ($this->user_panelmodel->create_interface($data)) {
+            $msg = array(
+                'status' => true,
+                'class' => 'successbox',
+                'msg' => 'Interface  Added successfully.'
+            );
+
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
         }
-       redirect('userpanel/setting_interface');
-	  
-	}
-	
-	  public function changepassword() {
-		$this->load->helper('form');
+        else
+        {
+            $msg = array(
+                'status' => false,
+                'class' => 'errormsgbox',
+                'msg' => 'Interface  Added Fail.'
+            );
+
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
+        }
+        redirect('userpanel/setting_interface');
+
+    }
+
+    public function changepassword()
+    {
+        $this->load->helper('form');
         $data = null;
         $error = null;
         $title = 'Change Password';
-        $this->template->write_view('content','template/user/pages/changepassword',array('data'=>$data,'error'=>$error,'title'=>$title));
+        $this->template->write_view('content', 'template/user/pages/changepassword', array('data' => $data, 'error' => $error, 'title' => $title));
         $this->template->render();
-	  
-	  }
-	  
-	  function changepass()
-	{   
-		$this->data=$_POST;
-		
-		$password_data = array(
-		'old_password'=>md5($this->input->post('old_password')),
-		'password'=>md5($this->input->post('password')),
-		'confirm_password'=>md5($this->input->post('confirm_password'))
-		);
-		
-		$old_password=$password_data["old_password"];
-		$user_password=$password_data["password"];
-		$confirm_password=$password_data["confirm_password"];
-		
-		$userinfo=$this->session->userdata('user_info');
-		$user_id=$userinfo['user_id'];
-		$this->load->model('user_panelmodel');
-	
-		if($this->input->post('password') || $this->input->post('old_password')){
-		if($this->user_panelmodel->check_old_password($user_id,$old_password))
-		{   
-			
-			if($user_password==$confirm_password)
-			{
-				if($this->user_panelmodel->update_password($user_id,$password_data))
-				{
-					
-					$msg = array(
-                        'status' => true,
-                        'class' => 'successbox',
-                        'msg' => 'Password Changed Successfully.'
-                    );
-                   $data = json_encode($msg);
-                   $this->session->set_flashdata('msg', $data);
-				}
-				else
-				{
-					$msg = array(
-                        'status' => true,
-                        'class' => 'errormsgbox',
-                        'msg' => 'There was some problem please try again.'
-                    );
-                   $data = json_encode($msg);
-                   $this->session->set_flashdata('msg', $data);
-					
-				}
-				
-			}
-			
-			
-			else
-			{
-				
-				$msg = array(
+
+    }
+
+    function changepass()
+    {
+        $this->data = $_POST;
+
+        $password_data = array(
+            'old_password' => md5($this->input->post('old_password')),
+            'password' => md5($this->input->post('password')),
+            'confirm_password' => md5($this->input->post('confirm_password'))
+        );
+
+        $old_password = $password_data["old_password"];
+        $user_password = $password_data["password"];
+        $confirm_password = $password_data["confirm_password"];
+
+        $userinfo = $this->session->userdata('user_info');
+        $user_id = $userinfo['user_id'];
+        $this->load->model('user_panelmodel');
+
+        if ($this->input->post('password') || $this->input->post('old_password')) {
+            if ($this->user_panelmodel->check_old_password($user_id, $old_password)) {
+
+                if ($user_password == $confirm_password) {
+                    if ($this->user_panelmodel->update_password($user_id, $password_data)) {
+
+                        $msg = array(
+                            'status' => true,
+                            'class' => 'successbox',
+                            'msg' => 'Password Changed Successfully.'
+                        );
+                        $data = json_encode($msg);
+                        $this->session->set_flashdata('msg', $data);
+                    }
+                    else
+                    {
+                        $msg = array(
+                            'status' => true,
+                            'class' => 'errormsgbox',
+                            'msg' => 'There was some problem please try again.'
+                        );
+                        $data = json_encode($msg);
+                        $this->session->set_flashdata('msg', $data);
+
+                    }
+
+                }
+
+
+                else
+                {
+
+                    $msg = array(
                         'status' => true,
                         'class' => 'errormsgbox',
                         'msg' => 'Please Type Same Password!'
                     );
-                   $data = json_encode($msg);
-                   $this->session->set_flashdata('msg', $data);
-			}
-		}
-		else
-		{
-			$msg = array(
-                        'status' => true,
-                        'class' => 'errormsgbox',
-                        'msg' => 'Wrong Old Password!'
-                    );
-                   $data = json_encode($msg);
-                   $this->session->set_flashdata('msg', $data);
-		}
-		}
-		else{
-			$msg = array(
-                        'status' => true,
-                        'class' => 'errormsgbox',
-                        'msg' => 'Please Type  Password!'
-                    );
-                   $data = json_encode($msg);
-                   $this->session->set_flashdata('msg', $data);
-		
-		}
-		redirect('userpanel/changepassword');
-	}
-	public function sender() {
-		$this->load->helper('form');
+                    $data = json_encode($msg);
+                    $this->session->set_flashdata('msg', $data);
+                }
+            }
+            else
+            {
+                $msg = array(
+                    'status' => true,
+                    'class' => 'errormsgbox',
+                    'msg' => 'Wrong Old Password!'
+                );
+                $data = json_encode($msg);
+                $this->session->set_flashdata('msg', $data);
+            }
+        }
+        else {
+            $msg = array(
+                'status' => true,
+                'class' => 'errormsgbox',
+                'msg' => 'Please Type  Password!'
+            );
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
+
+        }
+        redirect('userpanel/changepassword');
+    }
+
+    public function sender()
+    {
+        $this->load->helper('form');
         $data = null;
         $error = null;
         $title = 'Sender Add';
-	    $user_info = $this->session->userdata('user_info');
-	    $user_id=$user_info['user_id'];
-	    $this->load->model('user_panelmodel');
-		$sender_data=$this->user_panelmodel->show_senderdata($user_id);
-        $this->template->write_view('content','template/user/pages/sender',array('sender_data'=>$sender_data,'error'=>$error,'title'=>$title));
+        $user_info = $this->session->userdata('user_info');
+        $user_id = $user_info['user_id'];
+        $this->load->model('user_panelmodel');
+        $sender_data = $this->user_panelmodel->show_senderdata($user_id);
+        $this->template->write_view('content', 'template/user/pages/sender', array('sender_data' => $sender_data, 'error' => $error, 'title' => $title));
         $this->template->render();
-	  
-	  }
 
-	public function senderadd()
-	{
-	  $this->load->helper('form');
-	  $error = null;
-      $title = 'Interface Add';
-      $data = $this->input->post();
-	  $user_info = $this->session->userdata('user_info');
-	  $data['user_id']=$user_info['user_id'];
-	  $this->load->model('user_panelmodel');
-	  
-	if($this->user_panelmodel->sender_create($data)){
-		   $msg = array(
-                        'status' => true,
-                        'class' => 'successbox',
-                        'msg' => 'Sender Id Added successfully.'
-                    );
+    }
 
-        $data = json_encode($msg);
-        $this->session->set_flashdata('msg', $data);
-	  }	
-	  else
-	  {
-        $msg = array(
-                        'status' => false,
-                        'class' => 'errormsgbox',
-                        'msg' => 'Sender Id  Added Fail.'
-                    );
+    public function senderadd()
+    {
+        $this->load->helper('form');
+        $error = null;
+        $title = 'Interface Add';
+        $data = $this->input->post();
+        $user_info = $this->session->userdata('user_info');
+        $data['user_id'] = $user_info['user_id'];
+        $this->load->model('user_panelmodel');
 
-        $data = json_encode($msg);
-        $this->session->set_flashdata('msg', $data);
+        if ($this->user_panelmodel->sender_create($data)) {
+            $msg = array(
+                'status' => true,
+                'class' => 'successbox',
+                'msg' => 'Sender Id Added successfully.'
+            );
+
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
         }
-       redirect('userpanel/sender');
-	
-	
-	  
-	}
-	
-	 public function senderdelete($sender_id) {
-		$this->load->helper('form');
-        //$user_info = $this->session->userdata('user_info');
-	    //$user_id=$user_info['user_id'];
-	    $this->load->model('user_panelmodel');
-		
-	   if($this->user_panelmodel->senderdata_delete($sender_id)){
-		      $msg = array(
-                        'status' => true,
-                        'class' => 'successbox',
-                        'msg' => 'Sender Data Deleted successfully.'
-                    );
+        else
+        {
+            $msg = array(
+                'status' => false,
+                'class' => 'errormsgbox',
+                'msg' => 'Sender Id  Added Fail.'
+            );
 
-        $data = json_encode($msg);
-        $this->session->set_flashdata('msg', $data);
-	}
-	 else
-	  {
-        $msg = array(
-                        'status' => false,
-                        'class' => 'errormsgbox',
-                        'msg' => 'Sender data  Deleted Fail.'
-                    );
-
-        $data = json_encode($msg);
-        $this->session->set_flashdata('msg', $data);
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
         }
         redirect('userpanel/sender');
-	  }
-	  public function setdefault($sender_id) {
-		$this->load->helper('form');
+
+
+    }
+
+    public function senderdelete($sender_id)
+    {
+        $this->load->helper('form');
         //$user_info = $this->session->userdata('user_info');
-	    //$user_id=$user_info['user_id'];
-	    $this->load->model('user_panelmodel');
-		
-	   if($this->user_panelmodel->senderdata_update($sender_id)){
-		      $msg = array(
-                        'status' => true,
-                        'class' => 'successbox',
-                        'msg' => 'Sender Data Update successfully.'
-                    );
+        //$user_id=$user_info['user_id'];
+        $this->load->model('user_panelmodel');
 
-        $data = json_encode($msg);
-        $this->session->set_flashdata('msg', $data);
-	}
-	 else
-	  {
-        $msg = array(
-                        'status' => false,
-                        'class' => 'errormsgbox',
-                        'msg' => 'Sender data  Update Fail.'
-                    );
+        if ($this->user_panelmodel->senderdata_delete($sender_id)) {
+            $msg = array(
+                'status' => true,
+                'class' => 'successbox',
+                'msg' => 'Sender Data Deleted successfully.'
+            );
 
-        $data = json_encode($msg);
-        $this->session->set_flashdata('msg', $data);
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
+        }
+        else
+        {
+            $msg = array(
+                'status' => false,
+                'class' => 'errormsgbox',
+                'msg' => 'Sender data  Deleted Fail.'
+            );
+
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
         }
         redirect('userpanel/sender');
-	  }
+    }
+
+    public function setdefault($sender_id)
+    {
+        $this->load->helper('form');
+        //$user_info = $this->session->userdata('user_info');
+        //$user_id=$user_info['user_id'];
+        $this->load->model('user_panelmodel');
+
+        if ($this->user_panelmodel->senderdata_update($sender_id)) {
+            $msg = array(
+                'status' => true,
+                'class' => 'successbox',
+                'msg' => 'Sender Data Update successfully.'
+            );
+
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
+        }
+        else
+        {
+            $msg = array(
+                'status' => false,
+                'class' => 'errormsgbox',
+                'msg' => 'Sender data  Update Fail.'
+            );
+
+            $data = json_encode($msg);
+            $this->session->set_flashdata('msg', $data);
+        }
+        redirect('userpanel/sender');
+    }
+
 }
--- application/models/user_panelmodel.php
+++ application/models/user_panelmodel.php
@@ -1,113 +1,125 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class User_panelmodel extends CI_Model{
+class User_panelmodel extends CI_Model
+{
 
-	protected $errors;
+    protected $errors;
 
-	function __construct(){
-		parent::__construct();
-		$this->load->database();
-	}
-	
-	public function user_update($params){
-		$status = false;
-        $this->db->set($params); 
-		
-		if($this->db->update('smshub_users', $params, array('user_id' => $params['user_id'])));
-		$status = true;
-        return $status;
-	}
-	public function  user_data($user_id){
-		$query = $this->db->get_where('smshub_users', array('user_id' => $user_id));
-		$user_info=$query->row_array();
-		return $user_info;
-	}
-	public function create_interface($params){
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function user_update($params)
+    {
         $status = false;
         $this->db->set($params);
-        if($this->db->insert('smshub_interface'))
-		$status = true;
+
+        if ($this->db->update('smshub_users', $params, array('user_id' => $params['user_id']))) ;
+        $status = true;
         return $status;
-	}
-		
-	function check_old_password($user_id,$old_password)
-	{
-		$truefalse;
-		$this->db->from('smshub_users');
-		$this->db->where('user_id',$user_id);
-		$this->db->where('password',$old_password);
-		$query = $this->db->get();
-		$truefalse=$query->num_rows();
-		return $truefalse?true:false;
-	}
-	function update_password($user_id,$password_data)
-	{
-		unset($password_data['old_password']);
-		unset($password_data['confirm_password']);
-		$truefalse;
-		$this->db->where('user_id', $user_id);
-		if($this->db->update('smshub_users',$password_data))
-		{
-			$truefalse=TRUE;
-		}
-		else
-		{
-			$truefalse=FALSE;
-		}
-		//$this->db->last_query();
-		//exit();
-		return $truefalse;
-	}
-	public function sender_create($params){
+    }
+
+    public function  user_data($user_id)
+    {
+        $query = $this->db->get_where('smshub_users', array('user_id' => $user_id));
+        $user_info = $query->row_array();
+        return $user_info;
+    }
+
+    public function create_interface($params)
+    {
         $status = false;
         $this->db->set($params);
-        if($this->db->insert('smshub_sender'))
-		$status = true;
+        if ($this->db->insert('smshub_interface'))
+            $status = true;
         return $status;
-	}
- function show_senderdata($user_id)
-	{		
-		$this->db->from('smshub_sender');
-		$this->db->where('user_id',$user_id);
-		$this->db->order_by("sender_id", "Asc");
-		$query = $this->db->get();
-		//print_r($query->result());
-		//die(0);
-		return $query->result();
-	}
-	
-	public function senderdata_delete($sender_id){
+    }
+
+    function check_old_password($user_id, $old_password)
+    {
+        $truefalse;
+        $this->db->from('smshub_users');
+        $this->db->where('user_id', $user_id);
+        $this->db->where('password', $old_password);
+        $query = $this->db->get();
+        $truefalse = $query->num_rows();
+        return $truefalse ? true : false;
+    }
+
+    function update_password($user_id, $password_data)
+    {
+        unset($password_data['old_password']);
+        unset($password_data['confirm_password']);
+        $truefalse;
+        $this->db->where('user_id', $user_id);
+        if ($this->db->update('smshub_users', $password_data)) {
+            $truefalse = TRUE;
+        }
+        else
+        {
+            $truefalse = FALSE;
+        }
+        //$this->db->last_query();
+        //exit();
+        return $truefalse;
+    }
+
+    public function sender_create($params)
+    {
+        $status = false;
+        $this->db->set($params);
+        if ($this->db->insert('smshub_sender'))
+            $status = true;
+        return $status;
+    }
+
+    function show_senderdata($user_id)
+    {
+        $this->db->from('smshub_sender');
+        $this->db->where('user_id', $user_id);
+        $this->db->order_by("sender_id", "Asc");
+        $query = $this->db->get();
+        //print_r($query->result());
+        //die(0);
+        return $query->result();
+    }
+
+    public function senderdata_delete($sender_id)
+    {
         $status = false;
         $this->db->set($sender_id);
-        if($this->db->delete('smshub_sender', array('sender_id' => $sender_id)))
-		$status = true;
+        if ($this->db->delete('smshub_sender', array('sender_id' => $sender_id)))
+            $status = true;
         return $status;
-	}
-   function senderdata_update($sender_id)
-	{   $data = array(
-               'sender_status' =>1,
-           );
-	    $status_old = array(
-               'sender_status' =>0,
-           );
-		$truefalse;
-		$sender_status=1;
-		$this->db->where('sender_status', $sender_status);
-		$this->db->update('smshub_sender',$status_old);
-		$this->db->where('sender_id', $sender_id);
-		if($this->db->update('smshub_sender',$data))
-		{
-			$truefalse=TRUE;
-		}
-		else
-		{
-			$truefalse=FALSE;
-		}
-		//$this->db->last_query();
-		//exit();
-		return $truefalse;
-	}
-	
+    }
+
+    function senderdata_update($sender_id)
+    {
+        $data = array(
+            'sender_status' => 1,
+        );
+        $status_old = array(
+            'sender_status' => 0,
+        );
+        $truefalse;
+        $sender_status = 1;
+        $this->db->where('sender_status', $sender_status);
+        $this->db->update('smshub_sender', $status_old);
+        $this->db->where('sender_id', $sender_id);
+        if ($this->db->update('smshub_sender', $data)) {
+            $truefalse = TRUE;
+        }
+        else
+        {
+            $truefalse = FALSE;
+        }
+        //$this->db->last_query();
+        //exit();
+        return $truefalse;
+    }
+
 }
 
 
